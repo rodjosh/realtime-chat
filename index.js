d4cc0788bc260a13b988e69e8ffe2811abd84e7c
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2021-12-31 15:16:59 -0600

    Added libraries and instances

---
+++ index.js
@@ -0,0 +1,25 @@
+//Importing the necessary libraries
+const http = require('http');
+const socket_io = require('socket.io');
+const express = require('express');
+
+//Express intance
+const app = express();
+
+//HTTP Server
+const httpServer = http.createServer(app);
+
+//Socket.io server
+const io = new socket_io.Server();
+
+//Attach to the http server
+io.attach(httpServer, {
+
+	//To server client-side socket.io bundle
+	serveClient: true
+});
+
+//Listening from the http server
+httpServer.listen(3000, ()=>{
+	console.log('Listening on 3000');
+})
